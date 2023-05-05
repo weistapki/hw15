2379c14216e1cfe8f6b0ac9810fbb0c7d9cb88d2
weistapki <weistapki@gmail.com>
2023-05-05 16:56:15 +0200

    added solution

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class Calculator {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        String operation = args[2];
+
+        int result = 0;
+
+        switch (operation) {
+            case "+" -> result = sum(a, b);
+            case "-" -> result = subtract(a, b);
+            case "*" -> result = multiply(a, b);
+            case "/" -> result = divide(a, b);
+            default -> {
+                System.out.println("Invalid operation: " + operation);
+                System.exit(1);
+            }
+        }
+        System.out.println(result);
+    }
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+    private static int subtract(int a, int b) {
+        return a - b;
+    }
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
+    private static int divide(int a, int b) {
+        return a / b;
+    }
 }
